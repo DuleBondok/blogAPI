1cb1ebb146ed53f061d44897ec56f65d1edeec11
DuleBondok <dulebondok@gmail.com>
2025-03-05 13:42:20 +0100

    finished with welcome div

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -7,6 +7,18 @@ function Homepage() {
   return (
     <div>
       <Header />
+      <div className='underHeaderDiv'>
+        <div className='welcomeToDiv'>
+          <h1 className='welcomeToHeader'>Welcome to</h1>
+          <h1 className='blogsterHeader'>blogster</h1>
+          <p className='welcomeText'>Hello! My name is Dusan Bondokic. I am a full stack developer based in Nis, Serbia. This is my personal blog. Journey with me through my life's adventures, musings, and reflections.</p>
+          <div className='welcomeButtonsDiv'>
+            <button className='welcomeSignInBtn'>Sign Up</button>
+            <button className='gitHubBtn'><img src='./github.png' className='gitHubIcon'></img>GitHub</button>
+          </div>
+        </div>
+        <img src='./laptop.jpg' className='laptopImg'></img>
+      </div>
     </div>
   );
 }
